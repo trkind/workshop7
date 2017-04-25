19f892a18a5538c931bb27790e86e0ee0877f49d
Trent Kindvall <tgkindvall@umass.edu>
2017-04-25 16:07:41 -0400

    database-reset the real one

--- server/src/database.js
+++ server/src/database.js
@@ -1,68 +1,57 @@
+var ObjectID = require('mongodb').ObjectID;
+
 // Data goes here.
 var initialData = {
   // The "user" collection. Contains all of the users in our Facebook system.
   "users": {
     // This user has id "1".
     "1": {
-      "_id": 1,
+      "_id": new ObjectID("000000000000000000000001"),
       "fullName": "Someone",
-      "feed": 1
+      "feed": new ObjectID("000000000000000000000001")
     },
     "2": {
-      "_id": 2,
+      "_id": new ObjectID("000000000000000000000002"),
       "fullName": "Someone Else",
-      "feed": 2
+      "feed": new ObjectID("000000000000000000000002")
     },
     "3": {
-      "_id": 3,
+      "_id": new ObjectID("000000000000000000000003"),
       "fullName": "Another Person",
-      "feed": 3
+      "feed": new ObjectID("000000000000000000000003")
     },
     // This is "you"!
     "4": {
-      "_id": 4,
+      "_id": new ObjectID("000000000000000000000004"),
       "fullName": "John Vilk",
       // ID of your feed.
-      "feed": 4
+      "feed": new ObjectID("000000000000000000000004")
     }
   },
   // The 'feedItems' collection. Contains all of the feed items on our Facebook
   // system.
   "feedItems": {
     "1": {
-      "_id": 1,
-      // A list of users that liked the post. Here, "Someone Else" and "Another Person"
-      // liked this particular post.
+      "_id": new ObjectID("000000000000000000000001"),
       "likeCounter": [
-        2, 3
+        new ObjectID("000000000000000000000002"), new ObjectID("000000000000000000000003")
       ],
-      // The type and contents of this feed item. This item happens to be a status
-      // update.
       "type": "statusUpdate",
       "contents": {
-        // ID of the user that posted the status update.
-        "author": 1,
-        // 01/24/16 3:48PM EST, converted to Unix Time
-        // (# of milliseconds since Jan 1 1970 UTC)
-        // https://en.wikipedia.org/wiki/Unix_time
+        "author": new ObjectID("000000000000000000000001"),
         "postDate": 1453668480000,
         "location": "Austin, TX",
         "contents": "ugh."
       },
-      // List of comments on the post
       "comments": [
         {
-          // The author of the comment.
-          "author": 2,
-          // The contents of the comment.
+          "author": new ObjectID("000000000000000000000002"),
           "contents": "hope everything is ok!",
-          // The date the comment was posted.
-          // 01/24/16 22:00 EST
           "postDate": 1453690800000,
           "likeCounter": []
         },
         {
-          "author": 3,
+          "author": new ObjectID("000000000000000000000003"),
           "contents": "sending hugs your way",
           "postDate": 1453690800000,
           "likeCounter": []
@@ -70,11 +59,11 @@ var initialData = {
       ]
     },
     "2": {
-      "_id": 2,
+      "_id": new ObjectID("000000000000000000000002"),
       "likeCounter": [],
       "type": "statusUpdate",
       "contents": {
-        "author": 4,
+        "author": new ObjectID("000000000000000000000004"),
         "postDate": 1458231460117,
         "location": "Philadelphia, PA",
         "contents": "You can now edit and delete status updates!\nGo ahead and click the caret in the corner of the post."
@@ -85,20 +74,20 @@ var initialData = {
   // "feeds" collection. Feeds for each FB user.
   "feeds": {
     "4": {
-      "_id": 4,
+      "_id": new ObjectID("000000000000000000000004"),
       // Listing of FeedItems in the feed.
-      "contents": [2, 1]
+      "contents": [new ObjectID("000000000000000000000002"), new ObjectID("000000000000000000000001")]
     },
     "3": {
-      "_id": 3,
+      "_id": new ObjectID("000000000000000000000003"),
       "contents": []
     },
     "2": {
-      "_id": 2,
+      "_id": new ObjectID("000000000000000000000002"),
       "contents": []
     },
     "1": {
-      "_id": 1,
+      "_id": new ObjectID("000000000000000000000001"),
       "contents": []
     }
   }
